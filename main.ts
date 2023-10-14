586a72df7513886c075da563244c1b4dfc0c5cc5
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-10-14 21:49:16 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,13 +4,6 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
         basic.pause(1000)
     }
 })
-pins.onPulsed(DigitalPin.P1, PulseValue.Low, function () {
-    basic.turnRgbLedOff()
-    basic.showNumber(qwiicgpio.bitwise(qwiicgpio.readINPUT_PORT(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27)), qwiicgpio.eBit.RIGHT, 6))
-})
-pins.onPulsed(DigitalPin.P1, PulseValue.High, function () {
-    basic.setLedColor(0x0000ff)
-})
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     for (let Index = 0; Index <= 3; Index++) {
         qwiicgpio.writeOUTPUT_PORT(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27), Index * 16)
@@ -20,12 +13,25 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
 input.onButtonEvent(Button.A, ButtonEvent.Hold, function () {
     qwiicgpio.writeOUTPUT_PORT(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27), 0)
 })
+input.onButtonEvent(Button.B, ButtonEvent.Hold, function () {
+    basic.showNumber(qwiicgpio.readINPUT_PORT(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27)))
+})
+loops.everyInterval(1000, function () {
+    if (qwiicgpio.readINPUT_PORT(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27)) == 64) {
+        basic.setLedColor(0x0000ff)
+        for (let Index = 0; Index <= 31; Index++) {
+            qwiicgpio.writeOUTPUT_PORT(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27), Index)
+            basic.pause(1000)
+        }
+        basic.turnRgbLedOff()
+    }
+})
 basic.forever(function () {
-    qwiicgpio.beimStart(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27), true)
+    qwiicgpio.beimStart(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27))
     qwiicgpio.setMode(
     qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27),
-    qwiicgpio.eIO.IN,
-    qwiicgpio.eIO.IN,
+    qwiicgpio.eIO.IN_inverted,
+    qwiicgpio.eIO.IN_inverted,
     qwiicgpio.eIO.OUT,
     qwiicgpio.eIO.OUT,
     qwiicgpio.eIO.OUT,
